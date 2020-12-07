00bab3057243e7220fbbe260e2b1780fa0886e06
michdg-amzn <58176912+michdg-amzn@users.noreply.github.com>
2020-12-07 11:42:40 -0500

    Obfuscated bucket name

--- trigger-computeoptimizer-export/hello_world/app.py
+++ trigger-computeoptimizer-export/hello_world/app.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     client = boto3.client('compute-optimizer')
     response = client.export_ec2_instance_recommendations(
         s3DestinationConfig={
-            'bucket': 'recommendation-compute-ec2',
+            'bucket': '<bucket-name>',
             'keyPrefix': '/'
         },
         
